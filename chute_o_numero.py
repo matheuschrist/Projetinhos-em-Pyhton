bd7a1ad37f7972fb7a9f5a398fe066be313a9597
matheuschrist <matheusbrau12@gmail.com>
2022-07-01 14:51:08 -0300

    Finalizado Jogo do acerto

--- chute_o_numero.py
+++ chute_o_numero.py
@@ -1,5 +1,6 @@
 #Objetivo:acertar o Número
 import random
+import PySimpleGUI as sg
 
 class ChuteONumero:
     def __init__(self):
@@ -11,26 +12,47 @@ class ChuteONumero:
         
         
     def Iniciar(self):
+        #layout
+        self.layout=[
+            [sg.Text("Seu Chute",size=(35,0))],
+            [sg.Input(size=(20,0),key='ValorDoChute')],
+            [sg.Button('Chutar')],
+            [sg.Output(size=(20,20),key='saidaErro')]
+        ]
+        
+        self.layout2=[
+            [sg.Text("Você venceu",size=(35,0))]
+        ]
+       
         self.GerarNumeroAleatorio()
+         #Criar uma window
+        self.janela=sg.Window("Chute o Número",layout=self.layout)
+        
+        #receber valores
         self.Pergunta()
         try:
             while self.tentar_novamente==True:
                 if self.valor_do_chute > self.numero:
-                    print('Você errou ! ')
-                    print(' Chute um numero mais baixo !')
+                    print('Você errou !Chute um numero mais baixo ! ')
                     self.Pergunta()
                 elif self.valor_do_chute < self.numero:
-                    print('Você errou !')
-                    print(' Chute um numero mais alto !')
+                    print('Você errou !Chute um numero mais alto !')
                     self.Pergunta()
-                print('PARABÉNS VOCÊ ACERTOU!')
-                self.tentar_novamente=False
+                else:
+                    print('PARABÉNS VOCÊ ACERTOU!')
+                    self.tentar_novamente=False
+                    self.janela.Close()
+                    self.janelaVitoria=sg.Window("voce venceu",layout=self.layout2)
+                    self.evento=self.janelaVitoria.Read()
+                    
+                    
         except:
             print('Favor inserir apenas números!')    
             self.Iniciar()
     def Pergunta(self):
-         print('Chute um Número de 1 a 100')
-         self.valor_do_chute = int(input(''))
+        self.evento,self.valores = self.janela.Read()
+        if self.evento=='Chutar':
+            self.valor_do_chute=int(self.valores['ValorDoChute'])
     
     def GerarNumeroAleatorio(self):
          self.numero=random.randint(self.valor_minimo,self.valor_maximo)
