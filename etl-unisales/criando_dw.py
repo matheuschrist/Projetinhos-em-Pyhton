e4cb24f149f2dd8c5b1b0a80baf8d9dec1e856da
matheuschrist <matheusbrau12@gmail.com>
2022-08-20 19:36:24 -0300

    Enviando para o git

--- etl-unisales/criando_dw.py
+++ etl-unisales/criando_dw.py
@@ -3,27 +3,10 @@
 from transformacao import transformation as trans
 
 from sqlalchemy import create_engine
-import psycopg2
+
 #Carregamento no DW"""
-
 fato_orders,sallers,customers,geolocation=trans()
-"""
-# Update connection string information 
-host ="dw-unisales-dados.postgres.database.azure.com"
-dbname ="postgres"
-user = "dbaadmin"
-password =''
-sslmode ="require"
-# Construct connection string
 
-conn_string ="host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
-conn = psycopg2.connect(conn_string) 
-print("Connection established")
-#cursor = conn.cursor()
-
-"""
-
-"""https://docs.microsoft.com/pt-br/azure/postgresql/flexible-server/connect-python"""
 
 engine = create_engine(
     'postgresql://dbaadmin:{senha}@dw-unisales-dados.postgres.database.azure.com:5432/postgres')
